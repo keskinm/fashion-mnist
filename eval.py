1a9f2405339bd2a08151cfb3b6cf6349b0d43ac7
keskin <keskin.mustafa.42@gmail.com>
2019-08-25 09:30:29 +0200

    Refacto, add resume train

--- eval.py
+++ eval.py
@@ -15,12 +15,11 @@ logger = logging.getLogger(__name__)
 
 
 class FMModelsEvaluator:
-    def __init__(self, train_epoch, lr, train_batch_size, test, model_type, threshold_validation_accuracy, seed, save_dir):
+    def __init__(self, train_epoch, lr, train_batch_size, test_model, model_type, threshold_validation_accuracy, seed, save_dir, resume_model):
         self.train_epoch = train_epoch
         self.device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
         self.lr = lr[0]
         self.train_batch_size = train_batch_size
-        self.test = test
         self.model_type = model_type
         self.threshold_validation_accuracy = threshold_validation_accuracy
         if seed is not None:
@@ -30,6 +29,22 @@ class FMModelsEvaluator:
         self.save_model_dir_path = os.path.join(save_dir, 'models')
         os.makedirs(self.loss_plots_dir, exist_ok=True)
         os.makedirs(self.save_model_dir_path, exist_ok=True)
+
+        self.models = {'two_layers': TwoLayers(num_classes=10).to(self.device),
+                       'vgg16_pretrained': vgg16(pretrained=True, num_classes=1000).to(self.device),
+                       'vgg16': vgg16(num_classes=10).to(self.device)}
+
+        if test_model is not None:
+            self.test_model_name, self.model_to_test_path = test_model
+            self.test_model = self.models[self.test_model_name]
+        else:
+            self.model_to_test_path = None
+
+        if resume_model is not None:
+            self.resume_model_name, self.model_to_resume_path = resume_model
+            self.resume_model = self.models[self.resume_model_name]
+        else:
+            self.model_to_resume_path = None
 
     def prepare_data(self):
         transform = transforms.Compose([transforms.ToTensor()])
@@ -44,21 +59,12 @@ class FMModelsEvaluator:
 
         return train_set_loader, val_set_loader, test_set_loader
 
-    def eval(self):
-        train_set_loader, val_set_loader, test_set_loader = self.prepare_data()
-        # if self.model_type == 'vgg':
-        #     models_subtypes = [vgg16(pretrained=True, num_classes=1000).to(self.device),
-        #                        vgg16(num_classes=10).to(self.device)]
-        #
-        # elif self.model_type == 'two_layers':
-        #     models_subtypes = [TwoLayers(num_classes=10).to(self.device)]
-        #
-        # else:
-        #     models_subtypes = None
+    def eval(self, train_set_loader, val_set_loader):
+        if self.model_type:
+            models = {model_name: model for model_name, model in self.models.items() if self.model_type in model_name}
 
-        models = {'two_layers': TwoLayers(num_classes=10).to(self.device),
-                  'vgg16_pretrained': vgg16(pretrained=True, num_classes=1000).to(self.device),
-                  'vgg16': vgg16(num_classes=10).to(self.device)}
+        else:
+            models = self.models
 
         for model_name, model in models.items():
             optimizer = torch.optim.Adam(filter(lambda p: p.requires_grad, model.parameters()), lr=self.lr)
@@ -69,18 +75,35 @@ class FMModelsEvaluator:
             self.dump_metrics(accuracy, model_name)
 
     def run(self):
-        self.eval()
-        # self.load_model(None, None, './data/models/two_layers.pth')
-        # self.test(model, test_set_loader)
+        train_set_loader, val_set_loader, test_set_loader = self.prepare_data()
+
+        if self.model_to_test_path is not None:
+            model = self.load_model(model=self.test_model, optimizer=None, model_params_path=self.model_to_test_path)
+            self.compute_accuracy(model, test_set_loader)
+
+        elif self.model_to_resume_path is not None:
+            model = self.resume_model
+            optimizer = torch.optim.Adam(filter(lambda p: p.requires_grad, model.parameters()), lr=self.lr)
+            criterion = nn.CrossEntropyLoss()
+            model, optimizer = self.load_model(model=model, optimizer=optimizer, model_params_path=self.model_to_resume_path)
+            self.train(model, train_set_loader, optimizer, criterion, self.resume_model_name)
+
+        else:
+            self.eval(train_set_loader, val_set_loader)
 
     def train(self, model, train_set_loader, optimizer, criterion, model_name):
+        epoch_n = self.train_epoch
+        if isinstance(model, VGG):
+            epoch_n = 20
+
+        model = model.train()
         loss = None
         epoch = None
         losses = []
 
         # self.freeze_params(model)
 
-        for epoch in range(1, self.train_epoch + 1):
+        for epoch in range(1, epoch_n + 1):
             for batch_id, (image, label) in enumerate(train_set_loader):
                 print(batch_id)
                 label, image = label.to(self.device), image.to(self.device)
@@ -100,17 +123,15 @@ class FMModelsEvaluator:
         self.plot_losses(losses, model_name)
         self.save_model(epoch, loss, model, optimizer, model_name)
 
-        # return model
+        return model
 
     def save_model(self, epoch, loss, model, optimizer, model_name):
-        # save_model_file_path = os.path.join(self.save_model_dir_path, '{}.pth'.format(model_name))
-        # torch.save({
-        #     'epoch': epoch,
-        #     'model_state_dict': model.state_dict(),
-        #     'optimizer_state_dict': optimizer.state_dict(),
-        #     'loss': loss}, save_model_file_path)
         save_model_file_path = os.path.join(self.save_model_dir_path, '{}.pth'.format(model_name))
-        torch.save(model, save_model_file_path)
+        torch.save({
+            'epoch': epoch,
+            'model_state_dict': model.state_dict(),
+            'optimizer_state_dict': optimizer.state_dict(),
+            'loss': loss}, save_model_file_path)
 
     def plot_losses(self, losses, model_name):
         plt.plot(range(len(losses)), losses)
@@ -124,11 +145,12 @@ class FMModelsEvaluator:
         pass
 
     def compute_accuracy(self, model, data_set):
-        model.eval()
+        model = model.eval()
         with torch.no_grad():
             correct = 0
             total = 0
-            for image, label in data_set:
+            for batch_id, (image, label) in enumerate(data_set):
+                print("compute_acc", batch_id)
                 image = image.to(self.device)
                 label = label.to(self.device)
                 outputs = model(image)
@@ -136,8 +158,8 @@ class FMModelsEvaluator:
                 total += label.size(0)
                 correct += (predicted == label).sum().item()
             accuracy = 100 * correct / total
-            model.train()
-            return accuracy
+        print(accuracy)
+        return accuracy
 
     def dump_metrics(self, accuracy, model_name):
         metrics_file_path = os.path.join(self.save_dir, 'metrics.txt')
@@ -145,16 +167,17 @@ class FMModelsEvaluator:
             opened_metrics_file.write("model name:{} \naccuracy: {}\n\n".format(model_name, accuracy))
 
     def load_model(self, model, optimizer, model_params_path):
-        # model = model()
-        # optimizer = optimizer()
-        #
-        # checkpoint = torch.load(model_params_path)
-        # model.load_state_dict(checkpoint['model_state_dict'])
-        # optimizer.load_state_dict(checkpoint['optimizer_state_dict'])
-        # epoch = checkpoint['epoch']
-        # loss = checkpoint['loss']
-        model = torch.load(model_params_path)
-        state_dict = model.state_dict()
+        checkpoint = torch.load(model_params_path)
+        model.load_state_dict(checkpoint['model_state_dict'])
+
+        if self.model_to_resume_path is not None:
+            optimizer.load_state_dict(checkpoint['optimizer_state_dict'])
+            epoch = checkpoint['epoch']
+            loss = checkpoint['loss']
+            return model.to(self.device), optimizer
+
+        else:
+            return model.to(self.device)
 
 
 def main():
@@ -163,18 +186,23 @@ def main():
 
     parser.add_argument(
         '--model-type',
-        choices=['vgg, two_layers, sixlayers'],
+        choices=['vgg', 'two_layers', 'sixlayers'],
         default='two_layers',
         help=
         ''
     )
 
-    parser.add_argument(
-        '--test',
-        action='store_true',
-        help=
-        ''
-    )
+    parser.add_argument('-t',
+                        '--test-model',
+                        nargs='+',
+                        help='model path and model name',
+                        default=None)
+
+    parser.add_argument('-r',
+                        '--resume-model',
+                        nargs='+',
+                        help='model path and model name',
+                        default=None)
 
     parser.add_argument(
         '--train-batch-size',
