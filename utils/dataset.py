99387e7491cfaf3ed201b12a3a5ed6af66636b3c
keskin <keskin.mustafa.42@gmail.com>
2019-08-28 19:02:06 +0200

    Add dataset class

---
+++ utils/dataset.py
@@ -0,0 +1,14 @@
+import torch
+
+
+class DatasetTransformer(torch.utils.data.Dataset):
+    def __init__(self, base_dataset, transform):
+        self.base_dataset = base_dataset
+        self.transform = transform
+
+    def __getitem__(self, index):
+        img, target = self.base_dataset[index]
+        return self.transform(img), target
+
+    def __len__(self):
+        return len(self.base_dataset)
