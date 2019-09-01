e5a1bb6f0774624e467c417ea0c5cbfaf4ffa2f2
keskin <keskin.mustafa.42@gmail.com>
2019-09-01 18:05:44 +0200

    Add standardization functions to dataset.py

--- utils/dataset.py
+++ utils/dataset.py
@@ -1,14 +1,121 @@
 import torch
+import random
+import math
+from torchvision import transforms
+
+
+def compute_mean_std(loader):
+    # Compute the mean over minibatches
+    mean_img = None
+    for imgs, _ in loader:
+        if mean_img is None:
+            mean_img = torch.zeros_like(imgs[0])
+        mean_img += imgs.sum(dim=0)
+    mean_img /= len(loader.dataset)
+
+    # Compute the std over minibatches
+    std_img = torch.zeros_like(mean_img)
+    for imgs, _ in loader:
+        std_img += ((imgs - mean_img) ** 2).sum(dim=0)
+    std_img /= len(loader.dataset)
+    std_img = torch.sqrt(std_img)
+
+    # Set the variance of pixels with no variance to 1
+    # Because there is no variance
+    # these pixels will anyway have no impact on the final decision
+    std_img[std_img == 0] = 1
+
+    return mean_img, std_img
 
 
 class DatasetTransformer(torch.utils.data.Dataset):
-    def __init__(self, base_dataset, transform):
+    def __init__(self, base_dataset, transform, normalize, augment_prob=None):
         self.base_dataset = base_dataset
         self.transform = transform
+        self.normalize = normalize
+        self.augment_prob = augment_prob if augment_prob is not None else None
+        self.to_tensor_transform = transforms.Compose([transforms.ToTensor()])
 
     def __getitem__(self, index):
         img, target = self.base_dataset[index]
-        return self.transform(img), target
+
+        if self.augment_prob:
+            if random.random() < self.augment_prob:
+                img = self.transform(img)
+            else:
+                img = self.to_tensor_transform(img)
+
+        else:
+            img = self.transform(img)
+
+        if self.normalize:
+            return self.scale_tensor(img), target
+
+        else:
+            return img, target
+
+    @staticmethod
+    def scale_tensor(tensor):
+        min = tensor.min()
+        max = tensor.max()
+        tensor = (tensor - min) / (max - min)
+        return tensor
 
     def __len__(self):
         return len(self.base_dataset)
+
+
+class CenterReduce:
+    def __init__(self, mean, std):
+        self.mean = mean
+        self.std = std
+
+    def __call__(self, x):
+        return (x-self.mean)/self.std
+
+
+class RandomErasing(object):
+    '''
+    Class that performs Random Erasing in Random Erasing Data Augmentation by Zhong et al.
+    -------------------------------------------------------------------------------------
+    probability: The probability that the operation will be performed.
+    sl: min erasing area
+    sh: max erasing area
+    r1: min aspect ratio
+    mean: erasing value
+    -------------------------------------------------------------------------------------
+    '''
+    def __init__(self, probability = 0.5, sl = 0.02, sh = 0.4, r1 = 0.3, mean=[0.4914, 0.4822, 0.4465]):
+        self.probability = probability
+        self.mean = mean
+        self.sl = sl
+        self.sh = sh
+        self.r1 = r1
+
+    def __call__(self, img):
+
+        if random.uniform(0, 1) > self.probability:
+            return img
+
+        for attempt in range(100):
+            area = img.size()[1] * img.size()[2]
+
+            target_area = random.uniform(self.sl, self.sh) * area
+            aspect_ratio = random.uniform(self.r1, 1/self.r1)
+
+            h = int(round(math.sqrt(target_area * aspect_ratio)))
+            w = int(round(math.sqrt(target_area / aspect_ratio)))
+
+            if w <= img.size()[2] and h <= img.size()[1]:
+                x1 = random.randint(0, img.size()[1] - h)
+                y1 = random.randint(0, img.size()[2] - w)
+                if img.size()[0] == 3:
+                    img[0, x1:x1+h, y1:y1+w] = self.mean[0]
+                    img[1, x1:x1+h, y1:y1+w] = self.mean[1]
+                    img[2, x1:x1+h, y1:y1+w] = self.mean[2]
+                else:
+                    img[0, x1:x1+h, y1:y1+w] = self.mean[0]
+                return img
+
+        return img
+
