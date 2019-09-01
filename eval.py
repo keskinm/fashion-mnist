3bb6cf0a765153a6f58f99c496f8fb6a4ae7d000
keskin <keskin.mustafa.42@gmail.com>
2019-09-01 20:27:27 +0200

    Refactor eval

--- eval.py
+++ eval.py
@@ -3,12 +3,12 @@ from torch.utils.data import Dataset
 from torch import nn
 from torchvision import datasets, transforms
 import matplotlib.pyplot as plt
-from models.vgg import VGG, vgg16
-from models.two_conv_model import two_conv, two_conv_bn
-from models.five_conv_model import five_conv, five_conv_bn
+from models.vgg import VGG, vgg
+from models.two_conv_model import two_conv
+from models.five_conv_model import five_conv
 import logging
 from utils.logging import setup_logging, setup_argparse_logging_level
-from utils.dataset import DatasetTransformer, RandomErasing, CenterReduce, compute_mean_std
+from utils.dataset import DatasetTransformer, CenterReduce, compute_mean_std
 import argparse
 import time
 import os
@@ -17,9 +17,11 @@ logger = logging.getLogger(__name__)
 
 
 class FMModelsEvaluator:
-    def __init__(self, train_epoch, lr, train_batch_size, test_model,
-                 model_type, seed, save_dir, resume_model, optimizer,
-                 dump_metrics_frequency, threshold_validation_accuracy, num_threads, standardize, scale):
+    def __init__(self, train_epoch, lr, train_batch_size, test_model_path,
+                 model_type, model_cfg, seed, save_dir, resume_model_path,
+                 optimizer, dump_metrics_frequency,
+                 threshold_validation_accuracy, num_threads, standardize,
+                 scale, augment):
         self.train_epoch = train_epoch
         self.device = torch.device(
             'cuda' if torch.cuda.is_available() else 'cpu')
@@ -37,38 +39,8 @@ class FMModelsEvaluator:
         os.makedirs(self.save_model_dir_path, exist_ok=True)
         os.makedirs(self.metrics_dir_path, exist_ok=True)
 
-        self.models = {
-            'five_conv':
-            five_conv().to(self.device),
-            'five_conv_bn':
-            five_conv().to(self.device),
-            'five_conv_bn_augmented':
-            five_conv().to(self.device),
-            'two_conv':
-            two_conv().to(self.device),
-            'two_conv_bn':
-            two_conv_bn().to(self.device),
-            'two_conv_bn_augmented':
-            two_conv_bn().to(self.device),
-            'vgg16_pretrained':
-            vgg16(pretrained=True, num_classes=10).to(self.device),
-            'vgg16':
-            vgg16(num_classes=10).to(self.device),
-            'vgg16_pretrained_augmented':
-            vgg16(num_classes=10).to(self.device)
-        }
-
-        if test_model is not None:
-            self.test_model_name, self.model_to_test_path = test_model
-            self.test_model = self.models[self.test_model_name]
-        else:
-            self.model_to_test_path = None
-
-        if resume_model is not None:
-            self.resume_model_name, self.model_to_resume_path = resume_model
-            self.resume_model = self.models[self.resume_model_name]
-        else:
-            self.model_to_resume_path = None
+        self.model_to_test_path = test_model_path
+        self.model_to_resume_path = resume_model_path
 
         self.optimizer = optimizer
         self.dump_metrics_frequency = dump_metrics_frequency
@@ -79,6 +51,9 @@ class FMModelsEvaluator:
 
         self.train_set_loader, self.augmented_train_set_loader, self.val_set_loader, self.test_set_loader = self.prepare_data(
         )
+
+        self.model_cfg = model_cfg
+        self.augment = augment
 
     def prepare_data(self):
         pil_train_val_set = datasets.FashionMNIST('./data',
@@ -93,23 +68,30 @@ class FMModelsEvaluator:
                                              transform=None)
 
         if self.standardize:
-            standardazing_dataset = DatasetTransformer(pil_train_val_set, transforms.ToTensor(), self.scale)
-            standardizing_loader = torch.utils.data.DataLoader(dataset=standardazing_dataset,
-                                                             batch_size=self.train_batch_size,
-                                                             num_workers=self.num_threads)
+            standardazing_dataset = DatasetTransformer(pil_train_val_set,
+                                                       transforms.ToTensor(),
+                                                       self.scale)
+            standardizing_loader = torch.utils.data.DataLoader(
+                dataset=standardazing_dataset,
+                batch_size=self.train_batch_size,
+                num_workers=self.num_threads)
 
             # Compute mean and variance from the training set
-            mean_train_tensor, std_train_tensor = compute_mean_std(standardizing_loader)
+            mean_train_tensor, std_train_tensor = compute_mean_std(
+                standardizing_loader)
 
             normalization_function = CenterReduce(mean_train_tensor,
                                                   std_train_tensor)
 
-            to_tensor_transform = transforms.Compose([transforms.ToTensor(), transforms.Lambda(lambda x: normalization_function(x))])
+            to_tensor_transform = transforms.Compose([
+                transforms.ToTensor(),
+                transforms.Lambda(lambda x: normalization_function(x))
+            ])
             augment_to_tensor_transform = transforms.Compose([
-            transforms.RandomCrop(28, padding=0),
-            transforms.RandomHorizontalFlip(),
-            transforms.ToTensor(),
-            transforms.Lambda(lambda x: normalization_function(x))
+                transforms.RandomCrop(28, padding=0),
+                transforms.RandomHorizontalFlip(),
+                transforms.ToTensor(),
+                transforms.Lambda(lambda x: normalization_function(x))
             ])
 
         else:
@@ -121,17 +103,23 @@ class FMModelsEvaluator:
                 transforms.ToTensor(),
             ])
 
-        train_set = DatasetTransformer(pil_train_set, to_tensor_transform, self.scale)
+        train_set = DatasetTransformer(pil_train_set, to_tensor_transform,
+                                       self.scale)
         augmented_train_set = DatasetTransformer(pil_train_set,
-                                                 augment_to_tensor_transform, self.scale, augment_prob=0.5)
+                                                 augment_to_tensor_transform,
+                                                 self.scale,
+                                                 augment_prob=0.5)
 
-        valid_set = DatasetTransformer(pil_val_set, to_tensor_transform, self.scale)
-        test_set = DatasetTransformer(pil_test_set, to_tensor_transform, self.scale)
+        valid_set = DatasetTransformer(pil_val_set, to_tensor_transform,
+                                       self.scale)
+        test_set = DatasetTransformer(pil_test_set, to_tensor_transform,
+                                      self.scale)
 
-        train_set_loader = torch.utils.data.DataLoader(dataset=train_set,
-                                                       batch_size=self.train_batch_size,
-                                                       shuffle=True,
-                                                       num_workers=self.num_threads)
+        train_set_loader = torch.utils.data.DataLoader(
+            dataset=train_set,
+            batch_size=self.train_batch_size,
+            shuffle=True,
+            num_workers=self.num_threads)
 
         augmented_train_set_loader = torch.utils.data.DataLoader(
             augmented_train_set,
@@ -139,42 +127,53 @@ class FMModelsEvaluator:
             shuffle=True,
             num_workers=self.num_threads)
 
-        val_set_loader = torch.utils.data.DataLoader(dataset=valid_set,
-                                                     batch_size=50, shuffle=True,
-                                                     num_workers=self.num_threads)
+        val_set_loader = torch.utils.data.DataLoader(
+            dataset=valid_set,
+            batch_size=50,
+            shuffle=True,
+            num_workers=self.num_threads)
 
-        test_set_loader = torch.utils.data.DataLoader(dataset=test_set,
-                                                      batch_size=50,
-                                                      shuffle=False,
-                                                      num_workers=self.num_threads)
+        test_set_loader = torch.utils.data.DataLoader(
+            dataset=test_set,
+            batch_size=50,
+            shuffle=False,
+            num_workers=self.num_threads)
 
         return train_set_loader, augmented_train_set_loader, val_set_loader, test_set_loader
 
-    def eval(self):
-        if self.model_type:
-            models = {
-                model_name: model
-                for model_name, model in self.models.items()
-                if self.model_type in model_name
-            }
+    def define_model(self):
+        if self.model_type == 'vgg':
+            if self.model_cfg:
+                model = vgg(**self.model_cfg,
+                            cfg='A',
+                            progress=True,
+                            num_classes=10)
+            else:
+                model = vgg(cfg='A',
+                            batch_norm=False,
+                            pretrained=True,
+                            progress=True,
+                            num_classes=10)
+
+        elif self.model_type == 'five_conv':
+            self.model_cfg.pop('pretrained')
+            if self.model_cfg:
+                model = five_conv(**self.model_cfg, cfg='A')
+            else:
+                model = five_conv(cfg='A', batch_norm=False)
 
         else:
-            models = self.models
+            self.model_cfg.pop('pretrained')
+            if self.model_cfg:
+                model = two_conv(**self.model_cfg, cfg='A')
+            else:
+                model = two_conv(cfg='A', batch_norm=False)
+        return model.to(self.device)
 
-        for model_name, model in models.items():
-            train_set_loader = self.augmented_train_set_loader if 'augmented' in model_name else self.train_set_loader
-
-            criterion, optimizer = self.init_optimizer(model, model_name)
-            self.train(model=model,
-                       train_set_loader=train_set_loader,
-                       val_set_loader=self.val_set_loader,
-                       optimizer=optimizer,
-                       criterion=criterion,
-                       model_name=model_name)
-
-    def init_optimizer(self, model, model_name):
-        if isinstance(model, VGG) and 'pretrained' in model_name:
-            self.freeze_params(model)
+    def init_optimizer(self, model):
+        if isinstance(model, VGG):
+            if model.pretrained:
+                self.freeze_params(model)
 
         if self.optimizer == 'adam':
             optimizer = torch.optim.Adam(filter(lambda p: p.requires_grad,
@@ -191,8 +190,10 @@ class FMModelsEvaluator:
         return criterion, optimizer
 
     def run(self):
+        model = self.define_model()
+
         if self.model_to_test_path is not None:
-            model = self.load_model(model=self.test_model,
+            model = self.load_model(model=model,
                                     optimizer=None,
                                     model_params_path=self.model_to_test_path)
 
@@ -201,29 +202,36 @@ class FMModelsEvaluator:
             inference_time = time.time() - inference_time_start
             logging.info(inference_time)
 
-        elif self.model_to_resume_path is not None:
-            model = self.resume_model
-            train_set_loader = self.augmented_train_set_loader if 'augmented' in self.resume_model_name else self.train_set_loader
-            criterion, optimizer = self.init_optimizer(model,
-                                                       self.resume_model_name)
-            model, optimizer, losses, epoch = self.load_model(
-                model=model,
-                optimizer=optimizer,
-                model_params_path=self.model_to_resume_path)
-            self.train(model=model,
-                       train_set_loader=train_set_loader,
-                       val_set_loader=self.val_set_loader,
-                       optimizer=optimizer,
-                       criterion=criterion,
-                       model_name=self.resume_model_name,
-                       losses=losses,
-                       epoch_start_idx=epoch)
-
         else:
-            self.eval()
+            train_set_loader = self.augmented_train_set_loader if self.augment else self.train_set_loader
+
+            if self.model_to_resume_path is not None:
+                criterion, optimizer = self.init_optimizer(model)
+
+                model, optimizer, losses, epoch = self.load_model(
+                    model=model,
+                    optimizer=optimizer,
+                    model_params_path=self.model_to_resume_path)
+
+                self.train(model=model,
+                           train_set_loader=train_set_loader,
+                           val_set_loader=self.val_set_loader,
+                           optimizer=optimizer,
+                           criterion=criterion,
+                           losses=losses,
+                           epoch_start_idx=epoch)
+            else:
+                train_set_loader = self.augmented_train_set_loader if self.augment else self.train_set_loader
+
+                criterion, optimizer = self.init_optimizer(model)
+                self.train(model=model,
+                           train_set_loader=train_set_loader,
+                           val_set_loader=self.val_set_loader,
+                           optimizer=optimizer,
+                           criterion=criterion)
 
     def adjust_learning_rate(self, optimizer, epoch):
-        lr = self.lr * (0.1 ** (epoch // 30))
+        lr = self.lr * (0.1**(epoch // 30))
         for param_group in optimizer.param_groups:
             param_group['lr'] = lr
 
@@ -233,7 +241,6 @@ class FMModelsEvaluator:
               val_set_loader,
               optimizer,
               criterion,
-              model_name,
               losses=None,
               epoch_start_idx=0):
 
@@ -247,8 +254,8 @@ class FMModelsEvaluator:
         model = model.train()
 
         for epoch in range(epoch_start_idx + 1, epoch_n + 1):
+            self.adjust_learning_rate(optimizer, epoch)
             for batch_id, (image, label) in enumerate(train_set_loader):
-                self.adjust_learning_rate(optimizer, epoch)
                 logger.info(batch_id)
                 label, image = label.to(self.device), image.to(self.device)
                 output = model(image)
@@ -263,8 +270,7 @@ class FMModelsEvaluator:
                     accuracy = self.compute_accuracy(model, val_set_loader)
                     self.dump_metrics_and_save_model(accuracy, epoch, epoch_n,
                                                      loss, losses, model,
-                                                     model_name, optimizer,
-                                                     batch_id)
+                                                     optimizer, batch_id)
 
                     if accuracy >= self.threshold_validation_accuracy:
                         break
@@ -272,24 +278,22 @@ class FMModelsEvaluator:
                 accuracy = self.compute_accuracy(model, val_set_loader)
                 self.dump_metrics_and_save_model(accuracy, epoch, epoch_n,
                                                  loss, losses, model,
-                                                 model_name, optimizer,
-                                                 batch_id)
+                                                 optimizer, batch_id)
                 continue
 
             break
 
     def dump_metrics_and_save_model(self, accuracy, epoch, epoch_n, loss,
-                                    losses, model, model_name, optimizer,
-                                    batch_id):
-        self.dump_accuracy(accuracy, model_name, epoch, batch_id)
-        self.save_model(epoch, losses, model, optimizer, model_name)
-        self.plot_losses(losses, model_name)
+                                    losses, model, optimizer, batch_id):
+        self.dump_accuracy(accuracy, epoch, batch_id, model.model_name)
+        self.save_model(epoch, losses, model, optimizer)
+        self.plot_losses(losses, model.model_name)
         logger.info('Loss :{:.4f} Epoch[{}/{}]'.format(loss.item(), epoch,
                                                        epoch_n))
 
-    def save_model(self, epoch, losses, model, optimizer, model_name):
+    def save_model(self, epoch, losses, model, optimizer):
         save_model_file_path = os.path.join(self.save_model_dir_path,
-                                            '{}.pth'.format(model_name))
+                                            '{}.pth'.format(model.model_name))
         torch.save(
             {
                 'epoch': epoch,
@@ -331,7 +335,7 @@ class FMModelsEvaluator:
         logger.info(accuracy)
         return accuracy
 
-    def dump_accuracy(self, accuracy, model_name, epoch, batch_idx):
+    def dump_accuracy(self, accuracy, epoch, batch_idx, model_name):
         metrics_file_path = os.path.join(self.metrics_dir_path,
                                          '{}.txt'.format(model_name))
         with open(metrics_file_path, "a") as opened_metrics_file:
@@ -361,20 +365,19 @@ def main():
 
     parser.add_argument('--model-type',
                         choices=['vgg', 'two_conv', 'five_conv'],
-                        default=None,
                         help='')
 
     parser.add_argument('-t',
-                        '--test-model',
-                        nargs='+',
-                        help='model path and model name',
-                        default=None)
+                        '--test-model-path',
+                        default=None,
+                        type=str,
+                        help='model path')
 
     parser.add_argument('-r',
-                        '--resume-model',
-                        nargs='+',
-                        help='model path and model name',
-                        default=None)
+                        '--resume-model-path',
+                        default=None,
+                        type=str,
+                        help='model path')
 
     parser.add_argument('--train-batch-size', default=50, help='')
 
@@ -409,18 +412,36 @@ def main():
         type=int,
         help='Number of CPU to use for processing mini batches')
 
-    parser.add_argument(
-        '--scale',
-        action='store_true',
-        help='scale input in [0-1] range')
+    parser.add_argument('--scale',
+                        action='store_true',
+                        help='scale input in [0-1] range')
 
     parser.add_argument(
         '--standardize',
         action='store_true',
         help='Subtract each instance by mean of data and divide by std')
 
+    parser.add_argument('--augment',
+                        action='store_true',
+                        help='Use data augmentation')
+
+    parser.add_argument('--pretrained',
+                        action='store_true',
+                        help='Use pretrained weights for VGG')
+
+    parser.add_argument('--batch-norm',
+                        action='store_true',
+                        help='Use batch norm')
+
     args = parser.parse_args()
     args = vars(args)
+
+    model_cfg_keys = ('pretrained', 'batch_norm')
+    model_cfg = {k: args[k] for k in model_cfg_keys if k in args}
+    args['model_cfg'] = model_cfg
+    for key in model_cfg_keys:
+        args.pop(key)
+
     setup_logging(args.pop('logging_level'))
     evaluator = FMModelsEvaluator(**args)
     evaluator.run()
